1f059f465b19087ba21312bb244fdce21a3ca548
Blen Desta <destab@wwu.edu>
2017-06-06 19:22:22 -0700

    Changes stream graph, added zellie's comments and csv files and commeneted stream graph

--- draw.js
+++ draw.js
@@ -1,11 +1,14 @@
+//To make sure the visualization don't repeat on the same page
 var singleInstance = 0;
 
+//Get the user from the spinner
 function getUser() {
     var id = document.getElementById("partID");
     var user = id.options[id.selectedIndex].value;
     return user;
 }
 
+//Call the apropriate visualization depending on what vizualization is chosen from the radio buttons
 function runVisualization(user, vizualizationChosen) {
     if (vizualizationChosen === "Small Multiple") {
         smallMultiple(user);
@@ -25,14 +28,14 @@ function runVisualization(user, vizualizationChosen) {
     }
 }
 
-// Small Multiple
+// Call the Small Multiple Visualization file and pass in the user
 function smallMultiple(user) {
     window.localStorage.setItem("user", user);
     window.location.href = "smallMultiple.html";
 }
 
 
-//STACKED BAR CHART
+//Call the STACKED BAR CHART Visualization file and pass in the user
 function barChart() {
     var fileName = "csv/barChartData.csv";
     window.localStorage.setItem("fileName", fileName);
@@ -40,20 +43,20 @@ function barChart() {
 }
 
 
-//AVERAGE SMALL MULTIPLE
+//Call the AVERAGE SMALL MULTIPLE Visualization file and pass in the user
 function avgSmallMultiple() {
     var fileName = "csv/avgMultipleData.csv";
     window.localStorage.setItem("fileName", fileName);
     window.location.href = "avgSmallMultiple.html";
 }
 
-//STREAM GRAPH
+//Call the STREAM GRAPH Visualization file and pass in the user
 function streamGraph(user) {
     window.localStorage.setItem("user", user);
     window.location.href = "streamGraph.html";
 }
 
-//HEAT MAP
+//Call the HEAT MAP Visualization file and pass in the user
 function heatMap(user) {
     window.localStorage.setItem("user", user);
     window.location.href = "heatMap.html";
